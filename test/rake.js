9f46b5e29eaa63743a3c6b6886dc79aff4cfeee3
Ismael Hery <hery@lemonde.fr>
2014-04-19 12:48:42 +0200

    slight moving forward in candidates extraction

--- test/rake.js
+++ test/rake.js
@@ -6,6 +6,19 @@ describe('rake algorithm', function() {
 	var text = 'Compatibility of systems of linear constraints over the set of natural numbers\n' +
 		'Criteria of compatibility of a system of linear Diophantine equations, strict inequations, and nonstrict inequations are considered. Upper bounds for components of a minimal set of solutions and algorithms of construction of minimal generating sets of solutions for all types of systems are given. These criteria and the corresponding algorithms for constructing a minimal supporting set of solutions can be used in solving all the considered types of systems and systems of mixed types.';
 
+		it('should clean text', function(){
+
+			rake.cleanText('on  nettoie').should.be.eql('on nettoie');
+		});
+
+	it('should extract candidate keywords from a single sentence with no punctuation', function() {
+
+		var expected = ['Compatibility', 'systems', 'linear constraints'];
+
+		rake.candidates('Compatibility of systems and linear constraints').should.be.eql(expected);
+
+	});
+
 	it('should extract candidate keywords', function() {
 
 		var expected = ['Compatibility', 'systems', 'linear constraints', 'set', 'natural numbers',
