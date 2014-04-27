78e3864963143372612a23b2e80a8cc0e53dc670
Ismael Hery <hery@lemonde.fr>
2014-04-27 11:54:37 +0200

    before refactoring

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ rake
 Javascript implementation of the "Rake" keywords extraction algorithm
 
 
-Problem with Rake paper :
+# Problems with the Rake original scientific paper :
+
 * numbers is a stop word => natural numbers can not be a candidate keywords. I removed numbers from fox stop list
 * does not find mixed types as a candidate keywords. I've added mixed types as a candidates key words
 
--- test/rake.js
+++ test/rake.js
@@ -5,42 +5,53 @@ describe('rake algorithm', function() {
 
 	it('should extract candidate keywords from a single sentence with no punctuation', function() {
 
-		var expected = ['Compatibility', 'systems', 'linear constraints'];
+		var expected = {
+			candidates: [
+				{'Compatibility':['Compatibility']},
+				{'systems':['systems']},
+				{'linear constraints':['linear','constraints']},
+			components: [
+				{'Compatibility':{aloneFreq:1, totalFreq: 1},
+				{'systems':{aloneFreq:1, totalFreq: 1}},
+				{'linear':{aloneFreq:1, totalFreq: 1}},
+				{'constraints':{aloneFreq:1, totalFreq: 1}}]
+		};
 
 		rake.candidates('Compatibility of systems and linear constraints').should.be.eql(expected);
 
 	});
 
-	it('should extract candidate keywords from two sentences with punctuation', function() {
 
-		var expected = ['Compatibility', 'systems', 'linear constraints', 'class'];
+	// it('should extract candidate keywords from two sentences with punctuation', function() {
 
-		rake.candidates('Compatibility of systems. And also linear constraints. But with class.').should.be.eql(expected);
+	// 	var expected = ['Compatibility', 'systems', 'linear constraints', 'class'];
 
-	});
+	// 	rake.candidates('Compatibility of systems. And also linear constraints. But with class.').should.be.eql(expected);
 
-	it('should extract candidate keywords from two sentences with new line without punctuation', function() {
+	// });
 
-		var expected = ['Compatibility', 'systems', 'linear constraints', 'Class', 'unclassy'];
+	// it('should extract candidate keywords from two sentences with new line without punctuation', function() {
 
-		rake.candidates('Compatibility of systems. And also linear constraints\nClass or unclassy.').should.be.eql(expected);
+	// 	var expected = ['Compatibility', 'systems', 'linear constraints', 'Class', 'unclassy'];
 
-	});
+	// 	rake.candidates('Compatibility of systems. And also linear constraints\nClass or unclassy.').should.be.eql(expected);
 
-	it.only('should extract candidate keywords', function() {
+	// });
 
-		var text = 'Compatibility of systems of linear constraints over the set of natural numbers\n' +
-			'Criteria of compatibility of a system of linear Diophantine equations, strict inequations, and nonstrict inequations are considered. Upper bounds for components of a minimal set of solutions and algorithms of construction of minimal generating sets of solutions for all types of systems are given. These criteria and the corresponding algorithms for constructing a minimal supporting set of solutions can be used in solving all the considered types of systems and systems of mixed types.';
+	// it.only('should extract candidate keywords', function() {
 
-		var expected = ['Compatibility', 'systems', 'linear constraints', 'set', 'natural numbers',
-			'Criteria', 'compatibility', 'system', 'linear Diophantine equations', 'strict inequations',
-			'nonstrict inequations', 'considered', 'Upper bounds', 'components', 'minimal set', 'solutions', 'algorithms',
-			'construction', 'minimal generating sets', 'types', 'criteria', 'corresponding algorithms',
-			'constructing', 'minimal supporting set', 'solving', 'considered types', 'mixed types'
-		];
+	// 	var text = 'Compatibility of systems of linear constraints over the set of natural numbers\n' +
+	// 		'Criteria of compatibility of a system of linear Diophantine equations, strict inequations, and nonstrict inequations are considered. Upper bounds for components of a minimal set of solutions and algorithms of construction of minimal generating sets of solutions for all types of systems are given. These criteria and the corresponding algorithms for constructing a minimal supporting set of solutions can be used in solving all the considered types of systems and systems of mixed types.';
 
-		rake.candidates(text).should.be.eql(expected);
+	// 	var expected = ['Compatibility', 'systems', 'linear constraints', 'set', 'natural numbers',
+	// 		'Criteria', 'compatibility', 'system', 'linear Diophantine equations', 'strict inequations',
+	// 		'nonstrict inequations', 'considered', 'Upper bounds', 'components', 'minimal set', 'solutions', 'algorithms',
+	// 		'construction', 'minimal generating sets', 'types', 'criteria', 'corresponding algorithms',
+	// 		'constructing', 'minimal supporting set', 'solving', 'considered types', 'mixed types'
+	// 	];
 
-	});
+	// 	rake.candidates(text).should.be.eql(expected);
+
+	// });
 
 });
