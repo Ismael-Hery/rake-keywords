ebebe59163129929023fb841db290fd928d2fd19
Ismael Hery <hery@lemonde.fr>
2014-04-27 18:25:37 +0200

    at this stage it works as in the paper

--- test/rake.js
+++ test/rake.js
@@ -11,10 +11,10 @@ describe('rake algorithm', function() {
 				'systems':['systems'],
 				'linear constraints':['linear','constraints']},
 			components: {
-				'compatibility':{ aloneFreq:1, totalFreq: 1},
-				'systems':{ aloneFreq:1, totalFreq: 1},
-				'linear':{ aloneFreq:1, totalFreq: 1},
-				'constraints':{ aloneFreq:1, totalFreq: 1}}
+				'compatibility':{ frequency:1, degree: 1},
+				'systems':{ frequency:1, degree: 1},
+				'linear':{ frequency:1, degree: 2},
+				'constraints':{ frequency:1, degree: 2}}
 		};
 
 		rake.keywords('Compatibility of systems and linear constraints').should.be.eql(expected);
@@ -23,81 +23,87 @@ describe('rake algorithm', function() {
 
 	it('should extract candidate keywords from the original example text', function() {
 
-	var text = 'Compatibility of systems of linear constraints over the set of natural numbers\n' +
-			'Criteria of compatibility of a system of linear Diophantine equations, strict inequations, and nonstrict inequations are considered. Upper bounds for components of a minimal set of solutions and algorithms of construction of minimal generating sets of solutions for all types of systems are given. These criteria and the corresponding algorithms for constructing a minimal supporting set of solutions can be used in solving all the considered types of systems and systems of mixed types.';
+		var text = 'Compatibility of systems of linear constraints over the set of natural numbers\n' +
+				'Criteria of compatibility of a system of linear Diophantine equations, strict inequations, and nonstrict inequations are considered. Upper bounds for components of a minimal set of solutions and algorithms of construction of minimal generating sets of solutions for all types of systems are given. These criteria and the corresponding algorithms for constructing a minimal supporting set of solutions can be used in solving all the considered types of systems and systems of mixed types.';
 
-	// 	var expected = ['Compatibility', 'systems', 'linear constraints', 'set', 'natural numbers',
-	// 		'Criteria', 'compatibility', 'system', 'linear Diophantine equations', 'strict inequations',
-	// 		'nonstrict inequations', 'considered', 'Upper bounds', 'components', 'minimal set', 'solutions', 'algorithms',
-	// 		'construction', 'minimal generating sets', 'types', 'criteria', 'corresponding algorithms',
-	// 		'constructing', 'minimal supporting set', 'solving', 'considered types', 'mixed types'
-	// 	];
+		var expected = {
+				candidates: {
+					'compatibility':['compatibility'],
+					'systems':['systems'],
+					'linear constraints':['linear','constraints'],
+					'set':['set'],
+					'natural numbers':['natural','numbers'],
+					'criteria': ['criteria'],
+					'system': ['system'],
+					'linear diophantine equations': ['linear','diophantine','equations'],
+					'strict inequations':['strict','inequations'],
+					'nonstrict inequations':['nonstrict','inequations'],
+					'considered':['considered'],
+					'upper bounds':['upper', 'bounds'],
+					'components':['components'],
+					'minimal set':['minimal','set'],
+					'solutions':['solutions'],
+					'algorithms':['algorithms'],
+					'construction':['construction'],
+					'algorithms':['algorithms'],
+					'minimal generating sets':['minimal','generating','sets'],
+					'types':['types'],
+					'corresponding algorithms':['corresponding','algorithms'],
+					'constructing':['constructing'],
+					'minimal supporting set':['minimal','supporting','set'],
+					'solving':['solving'],
+					'considered types':['considered','types'],
+					'mixed types':['mixed','types'],
+				},
+				components: {
+					'compatibility':{ frequency:2, degree: 2},
+					'systems':{ frequency:4, degree: 4},
+					'linear':{ frequency:2, degree: 5},
+					'constraints':{ frequency:1, degree: 2},
+					'set':{ frequency:3, degree: 6},
+					'natural':{ frequency:1, degree: 2},
+					'numbers':{ frequency:1, degree: 2},
+					'criteria':{ frequency:2, degree: 2},
+					'system':{ frequency:1, degree: 1},
+					'diophantine':{ frequency:1, degree: 3},
+					'equations':{ frequency:1, degree: 3},
+					'strict':{ frequency:1, degree: 2},
+					'inequations':{ frequency:2, degree: 4},
+					'nonstrict':{ frequency:1, degree: 2},
+					'considered':{ frequency:2, degree: 3},
+					'upper':{ frequency:1, degree: 2},
+					'bounds':{ frequency:1, degree: 2},
+					'components':{ frequency:1, degree: 1},
+					'minimal':{ frequency:3, degree: 8},
+					'solutions':{ frequency:3, degree: 3},
+					'algorithms':{ frequency:2, degree: 3},
+					'construction':{ frequency:1, degree: 1},
+					'generating':{ frequency:1, degree: 3},
+					'sets':{ frequency:1, degree: 3},
+					'types':{ frequency:3, degree: 5},
+					'corresponding':{ frequency:1, degree: 2},
+					'constructing':{ frequency:1, degree: 1},
+					'supporting':{ frequency:1, degree: 3},
+					'solving':{ frequency:1, degree: 1},
+					'mixed':{ frequency:1, degree: 2},
+				}
+			};
 
-	var expected = {
-			candidates: {
-				'compatibility':['compatibility'],
-				'systems':['systems'],
-				'linear constraints':['linear','constraints'],
-				'set':['set'],
-				'natural numbers':['natural','numbers'],
-				'criteria': ['criteria'],
-				'system': ['system'],
-				'linear diophantine equations': ['linear','diophantine','equations'],
-				'strict inequations':['strict','inequations'],
-				'nonstrict inequations':['nonstrict','inequations'],
-				'considered':['considered'],
-				'upper bounds':['upper', 'bounds'],
-				'components':['components'],
-				'minimal set':['minimal','set'],
-				'solutions':['solutions'],
-				'algorithms':['algorithms'],
-				'construction':['construction'],
-				'algorithms':['algorithms'],
-				'minimal generating sets':['minimal','generating','sets'],
-				'types':['types'],
-				'corresponding algorithms':['corresponding','algorithms'],
-				'constructing':['constructing'],
-				'minimal supporting set':['minimal','supporting','set'],
-				'solving':['solving'],
-				'considered types':['considered','types'],
-				'mixed types':['mixed','types'],
-			},
-			components: {
-				'compatibility':{ aloneFreq:2, totalFreq: 2},
-				'systems':{ aloneFreq:4, totalFreq: 4},
-				'linear':{ aloneFreq:1, totalFreq: 2},
-				'constraints':{ aloneFreq:1, totalFreq: 1},
-				'set':{ aloneFreq:1, totalFreq: 3},
-				'natural':{ aloneFreq:1, totalFreq: 1},
-				'numbers':{ aloneFreq:1, totalFreq: 1},
-				'criteria':{ aloneFreq:2, totalFreq: 2},
-				'system':{ aloneFreq:1, totalFreq: 1},
-				'diophantine':{ aloneFreq:1, totalFreq: 1},
-				'equations':{ aloneFreq:1, totalFreq: 1},
-				'strict':{ aloneFreq:1, totalFreq: 1},
-				'inequations':{ aloneFreq:1, totalFreq: 2},
-				'nonstrict':{ aloneFreq:1, totalFreq: 1},
-				'considered':{ aloneFreq:1, totalFreq: 2},
-				'upper':{ aloneFreq:1, totalFreq: 1},
-				'bounds':{ aloneFreq:1, totalFreq: 1},
-				'components':{ aloneFreq:1, totalFreq: 1},
-				'minimal':{ aloneFreq:1, totalFreq: 3},
-				'solutions':{ aloneFreq:3, totalFreq: 3},
-				'algorithms':{ aloneFreq:1, totalFreq: 2},
-				'construction':{ aloneFreq:1, totalFreq: 1},
-				'generating':{ aloneFreq:1, totalFreq: 1},
-				'sets':{ aloneFreq:1, totalFreq: 1},
-				'types':{ aloneFreq:1, totalFreq: 3},
-				'corresponding':{ aloneFreq:1, totalFreq: 1},
-				'constructing':{ aloneFreq:1, totalFreq: 1},
-				'supporting':{ aloneFreq:1, totalFreq: 1},
-				'solving':{ aloneFreq:1, totalFreq: 1},
-				'mixed':{ aloneFreq:1, totalFreq: 1},
-			}
-		};
-
-	rake.keywords(text).should.be.eql(expected);
-
+		rake.keywords(text).should.be.eql(expected);
 	});
+
+  it('should compute scores', function() {
+
+    var text = 'Compatibility of systems of linear constraints over the set of natural numbers\n' +
+        'Criteria of compatibility of a system of linear Diophantine equations, strict inequations, and nonstrict inequations are considered. Upper bounds for components of a minimal set of solutions and algorithms of construction of minimal generating sets of solutions for all types of systems are given. These criteria and the corresponding algorithms for constructing a minimal supporting set of solutions can be used in solving all the considered types of systems and systems of mixed types.';
+
+    var scores = rake.keywordsAndScores(text);
+
+    console.log(scores);
+
+    scores['minimal generating sets'].should.be.eql(8.7);
+    scores['linear diophantine equations'].should.be.eql(8.5);
+    scores['minimal supporting set'].should.be.eql(7.7);
+  });
 
 });
