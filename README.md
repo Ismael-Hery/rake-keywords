215839beb1275ac9a3eaf8e49dad43485a7e7c03
Ismael Hery <hery@lemonde.fr>
2014-04-27 11:42:54 +0200

    candidate keywords extraction ok (lots of errors in original sci paper)

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 rake
 ====
 
-Javascript implementation of the "Rake" keywords extraction algorithm 
+Javascript implementation of the "Rake" keywords extraction algorithm
+
+
+Problem with Rake paper :
+* numbers is a stop word => natural numbers can not be a candidate keywords. I removed numbers from fox stop list
+* does not find mixed types as a candidate keywords. I've added mixed types as a candidates key words
+
+# TODO :
+
+* Use a better stop word list
+* French implementation with 'mots de liaisons' du/des/... excluded from stop list
--- test/rake.js
+++ test/rake.js
@@ -3,14 +3,6 @@ var rake = require('../lib/rake');
 
 describe('rake algorithm', function() {
 
-	var text = 'Compatibility of systems of linear constraints over the set of natural numbers\n' +
-		'Criteria of compatibility of a system of linear Diophantine equations, strict inequations, and nonstrict inequations are considered. Upper bounds for components of a minimal set of solutions and algorithms of construction of minimal generating sets of solutions for all types of systems are given. These criteria and the corresponding algorithms for constructing a minimal supporting set of solutions can be used in solving all the considered types of systems and systems of mixed types.';
-
-		it('should clean text', function(){
-
-			rake.cleanText('on  nettoie').should.be.eql('on nettoie');
-		});
-
 	it('should extract candidate keywords from a single sentence with no punctuation', function() {
 
 		var expected = ['Compatibility', 'systems', 'linear constraints'];
@@ -19,13 +11,32 @@ describe('rake algorithm', function() {
 
 	});
 
-	it('should extract candidate keywords', function() {
+	it('should extract candidate keywords from two sentences with punctuation', function() {
+
+		var expected = ['Compatibility', 'systems', 'linear constraints', 'class'];
+
+		rake.candidates('Compatibility of systems. And also linear constraints. But with class.').should.be.eql(expected);
+
+	});
+
+	it('should extract candidate keywords from two sentences with new line without punctuation', function() {
+
+		var expected = ['Compatibility', 'systems', 'linear constraints', 'Class', 'unclassy'];
+
+		rake.candidates('Compatibility of systems. And also linear constraints\nClass or unclassy.').should.be.eql(expected);
+
+	});
+
+	it.only('should extract candidate keywords', function() {
+
+		var text = 'Compatibility of systems of linear constraints over the set of natural numbers\n' +
+			'Criteria of compatibility of a system of linear Diophantine equations, strict inequations, and nonstrict inequations are considered. Upper bounds for components of a minimal set of solutions and algorithms of construction of minimal generating sets of solutions for all types of systems are given. These criteria and the corresponding algorithms for constructing a minimal supporting set of solutions can be used in solving all the considered types of systems and systems of mixed types.';
 
 		var expected = ['Compatibility', 'systems', 'linear constraints', 'set', 'natural numbers',
 			'Criteria', 'compatibility', 'system', 'linear Diophantine equations', 'strict inequations',
 			'nonstrict inequations', 'considered', 'Upper bounds', 'components', 'minimal set', 'solutions', 'algorithms',
 			'construction', 'minimal generating sets', 'types', 'criteria', 'corresponding algorithms',
-			'constructing', 'minimal supporting set', 'solving', 'considered types','mixed types'
+			'constructing', 'minimal supporting set', 'solving', 'considered types', 'mixed types'
 		];
 
 		rake.candidates(text).should.be.eql(expected);
