7a988bb1d7e4d0c945c7cdc32719521ef843013b
Ismael Hery <hery@lemonde.fr>
2014-04-28 21:35:17 +0200

    clean test files

--- test-index.js
+++
@@ -1,8 +0,0 @@
-var rake = require('./index.js');
-var stops = require ('./lib/englishStops').words;
-var fs = require('fs');
-
-var text = fs.readFileSync('guardian.txt').toString();
-
-
-console.log(rake.keywordsAndScores(text, stops));
